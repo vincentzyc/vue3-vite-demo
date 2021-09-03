3cb1e378670ff96e8584e8398212b0bae62fc11f
vincentzyc <vincentzyc@qq.com>
2021-09-03 17:41:37 +0800

    🎉 init: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <Button color="#7232dd">单色按钮</Button>
+  <Button color="#7232dd" plain>单色按钮</Button>
+  <Button color="linear-gradient(to right, #ff6034, #ee0a24)">渐变色按钮</Button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Button } from 'vant';
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  server: {
+    port: 3030
+  },
+  build: {
+    target: "es2015"
+  },
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'vant',
+          esModule: true,
+          resolveStyle: (name) => `vant/es/${name}/style`,
+        },
+      ],
+    })
+  ],
+})
