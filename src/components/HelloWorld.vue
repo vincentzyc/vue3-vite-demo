d22081ceeab669a513f5673a31d0e01f02dccdb6
vincentzyc <vincentzyc@qq.com>
2021-09-05 17:37:16 +0800

    🎉 init: init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,18 @@
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <!-- 优惠券单元格 -->
+  <CouponCell :coupons="coupons" :chosen-coupon="chosenCoupon" @click="showList = true" />
+  <!-- 优惠券列表 -->
+  <Popup v-model:show="showList" round position="bottom" style="height: 90%; padding-top: 4px;">
+    <CouponList
+      :coupons="coupons"
+      :chosen-coupon="chosenCoupon"
+      :disabled-coupons="disabledCoupons"
+      @change="onChange"
+      @exchange="onExchange"
+    />
+  </Popup>
   <Button color="#7232dd">单色按钮</Button>
   <Button color="#7232dd" plain>单色按钮</Button>
   <Button color="linear-gradient(to right, #ff6034, #ee0a24)">渐变色按钮</Button>
@@ -13,11 +25,38 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Button } from 'vant';
+import { Button, CouponCell, Popup, CouponList } from 'vant';
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+const coupon = {
+  id: 111,
+  available: 1,
+  condition: '无使用门槛\n最多优惠12元',
+  description: '描述信息，优惠券可用时展示',
+  reason: '',
+  value: 150,
+  name: '优惠券名称',
+  startAt: 1489104000,
+  endAt: 1514592000,
+  valueDesc: '1.5',
+  unitDesc: '元',
+};
+
+const coupons = ref([coupon]);
+const showList = ref(false);
+const chosenCoupon = ref(-1);
+
+const onChange = (index: number) => {
+  showList.value = false;
+  chosenCoupon.value = index;
+};
+const onExchange = (code: any) => {
+  coupons.value.push(coupon);
+};
+const disabledCoupons = [coupon]
 </script>
 
 
