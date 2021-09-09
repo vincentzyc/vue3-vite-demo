31ff15309cb14689782435eeab3fd854bfe113c0
vincentzyc <vincentzyc@qq.com>
2021-09-09 15:18:12 +0800

    🎉 init: init

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,19 @@ export default defineConfig({
   build: {
     target: "es2015",
     // rollupOptions: {
-    //   // 确保外部化处理那些你不想打包进库的依赖
-    //   external: ['html2canvas'],
-    //   output: {
-    //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-    //     globals: {
-    //       html2canvas: 'html2canvas'
-    //     }
-    //   }
+      // 确保外部化处理那些你不想打包进库的依赖
+      // external: ['vue', 'html2canvas'],
+      // output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        // globals: {
+        //   vue: 'Vue',
+        //   html2canvas: 'html2canvas'
+        // },
+        // paths: {
+        //   vue: 'https://cdn.jsdelivr.net/npm/vue@3.2.6/dist/vue.global.min.js',
+        //   html2canvas: 'https://cdn.jsdelivr.net/npm/html2canvas@1.3.2/dist/html2canvas.min.js'
+        // }
+      // }
     // }
   },
   plugins: [
