9ad85430ffc97a7856cabf122abb7a97991e1239
vincentzyc <vincentzyc@qq.com>
2021-09-13 09:44:21 +0800

    🎉 init: init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,8 +20,8 @@
   </Popup>
   <Button color="#7232dd">单色按钮</Button>
   <Button color="#7232dd" plain>单色按钮</Button>
-
   <Button color="linear-gradient(to right, #ff6034, #ee0a24)" @click="pageCanvas()">html2canvas</Button>
+  
   <Popup v-model:show="showCanvas" round position="center" style="height: 90%; padding-top: 4px;">
     <div id="canvasWrap"></div>
   </Popup>
