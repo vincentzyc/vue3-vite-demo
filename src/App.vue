cf7ca4bd93a63eb66b8e883a76a40a10af491551
vincentzyc <vincentzyc@qq.com>
2021-09-14 09:36:55 +0800

    🎉 init: init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+window.addHistoryListener('history', function () {
+  console.log('窗口history改变，我也听到了')// 可绑定多个监听事件
+  console.log(history)
+})
+// history.pushState({foo：bar}, 'title', '/car')
+</script>
 
 <style>
 #app {
