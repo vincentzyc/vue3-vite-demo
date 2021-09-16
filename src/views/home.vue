ab3abf8f772a698d6d4de7486074ecab57ae7293
vincentzyc <vincentzyc@qq.com>
2021-09-16 09:09:12 +0800

    🎉 init: init

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 style="text-align: center;">Home</h1>
+    <button @click="gotest">test</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { routerPush } from "../router";
+
+const gotest = ()=>{
+  routerPush('test')
+}
+</script>
