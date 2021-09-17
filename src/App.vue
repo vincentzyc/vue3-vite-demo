13d4e18a267dafc928ef027e84eb74e5df5b3e3b
vincentzyc <vincentzyc@qq.com>
2021-09-17 10:12:03 +0800

    🎉 init: init

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,13 @@
 
 <script setup lang="ts">
 import { shallowRef } from "vue"
-import { pageList } from "./router"
-import { getUrlParam } from "./utils"
+import { getCurPage } from "./router"
 
 let curPgae = shallowRef()
 
-function getCurPage() {
-  const page = getUrlParam('page') || 'home'
-  const comp = pageList.find(v => v.name === page)
-  if (comp) {
-    curPgae.value = comp.component ? comp.component : ''
-  } else {
-    const comp404 = pageList.find(v => v.name === '404')
-    if (comp404) {
-      curPgae.value = comp404.component ? comp404.component : ''
-    } else {
-      curPgae.value = ''
-    }
-  }
-}
-getCurPage()
+curPgae.value = getCurPage()
 
-window.addHistoryListener(() => getCurPage())
-window.addEventListener('popstate', () => getCurPage())
+window.addHistoryListener(() => curPgae.value = getCurPage())
+window.addEventListener('popstate', () => curPgae.value = getCurPage())
 
 </script>
