951e9e2df3d36924e8c50b1b11242ea7fe7aa806
vincentzyc <vincentzyc@qq.com>
2021-09-17 09:53:04 +0800

    🎉 init: init

--- src/views/haha.vue
+++ src/views/haha.vue
@@ -1,4 +1,17 @@
 <template>
   <img alt="Vue logo" src="../assets/logo.png" />
   <h1>hahaha</h1>
+  <button @click="gohome">home</button>
+  <button @click="go404">404</button>
 </template>
+
+
+<script lang="ts" setup>
+import { routerPush } from "../router";
+
+const gohome = () => {
+  routerPush('home')
+}
+
+const go404 = () => routerPush('404')
+</script>
