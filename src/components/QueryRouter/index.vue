54f44fad9643d13147bdbf2e2f24481080abb3e9
vincentzyc <vincentzyc@qq.com>
2021-09-19 22:27:00 +0800

    🎉 init: init

---
+++ src/components/QueryRouter/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <component v-if="curPgae" :is="curPgae" />
+</template>
+
+<script setup lang="ts">
+import { shallowRef } from "vue"
+import { getCurPage } from "./router"
+
+defineProps<{ list: string }>()
+
+const curPgae = shallowRef()
+
+curPgae.value = getCurPage()
+
+window.addHistoryListener(() => curPgae.value = getCurPage())
+window.addEventListener('popstate', () => curPgae.value = getCurPage())
+
+</script>
