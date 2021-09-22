ca4c5ed41e07ddfff2ecac73f40e6ef8984aeb51
vincentzyc <vincentzyc@qq.com>
2021-09-22 09:52:55 +0800

    🎉 init: init

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script lang="ts" setup>
-import { routerPush } from "../router";
+import { routerPush } from "../components/QueryRouter/router";
 
 const gotest = ()=>{
   routerPush('test')
