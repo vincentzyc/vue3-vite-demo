0ed13bb0388731406c5269e91c41ae0b7808e9ca
vincentzyc <vincentzyc@qq.com>
2021-09-28 09:11:29 +0800

    🎉 init: init

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,10 +2,12 @@
   <div>
     <h1 style="text-align: center;">Home</h1>
     <button @click="gotest">test</button>
+    <ContactCard />
   </div>
 </template>
 
 <script lang="ts" setup>
+import ContactCard from "@/components/ContactCard.vue"
 import { QRouter } from "@/QueryRouter"
 
 const gotest = () => {
