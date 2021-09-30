acc0ef0da7823fc4c51e45f6dab9bdb5529d6a46
vincentzyc <vincentzyc@qq.com>
2021-09-30 08:56:17 +0800

    🎉 init: init

--- src/views/haha.vue
+++ src/views/haha.vue
@@ -3,11 +3,16 @@
   <h1>hahaha</h1>
   <button @click="gohome">home</button>
   <button @click="go404">404</button>
+  <CellGroup inset>
+    <Cell title="单元格" value="内容" />
+    <Cell title="单元格" value="内容" label="描述信息" />
+  </CellGroup>
 </template>
 
 
 <script lang="ts" setup>
 import { QRouter } from "@/QueryRouter"
+import { Cell, CellGroup } from "vant";
 
 const gohome = () => {
   QRouter.push('home')
