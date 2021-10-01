45b3e868e93f46de2a74ebb5991c94527b53c21f
vincentzyc <vincentzyc@qq.com>
2021-10-02 07:48:01 +0800

    🎉 init: init

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <Swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <SwipeItem>1</SwipeItem>
+      <SwipeItem>2</SwipeItem>
+      <SwipeItem>3</SwipeItem>
+      <SwipeItem>4</SwipeItem>
+    </Swipe>
     <h1 style="text-align: center;">Home</h1>
     <button @click="gotest">test</button>
     <ContactCard />
@@ -7,6 +13,7 @@
 </template>
 
 <script lang="ts" setup>
+import { Swipe, SwipeItem } from "vant"
 import ContactCard from "@/components/ContactCard.vue"
 import { QRouter } from "@/QueryRouter"
 
